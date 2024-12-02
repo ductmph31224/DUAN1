3a3f7bd6977eb619ee6b7da607dfb28a0d439a10
ductmph31224 <147308104+ductmph31224@users.noreply.github.com>
2024-12-02 19:55:11 +0700

    sua loi giao dien

--- resources/views/client/index.blade.php
+++ resources/views/client/index.blade.php
@@ -91,38 +91,9 @@
             font-size: 1re;
         }
     </style>
-
     <!-- banner-area-end -->
     <!-- slider-area-start -->
-    <div class="slider-area">
-        <div class="slider-active owl-carousel">
-            <div class="single-slider pt-125 pb-130 bg-img" style="background-image:url(/client/img/slider/1.jpg);">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-lg-5">
-                            <div class="slider-content slider-animated-1 text-center">
-                                <h1>Huge Sale</h1>
-                                <h2>koparion</h2>
-                                <h3>Now starting at £99.00</h3>
-                                <a href="#">Shop now</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="single-slider slider-h1-2 pt-215 pb-100 bg-img"
-                style="background-image:url(/client/img/slider/2.jpg);">
-                <div class="container">
-                    <div class="slider-content slider-content-2 slider-animated-1">
-                        <h1>We can help get your</h1>
-                        <h2>Books in Order</h2>
-                        <h3>and Accessories</h3>
-                        <a href="#">Contact Us Today!</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+   
     <div class="banner-area banner-res-large pt-30 pb-5">
         <div class="container">
             <div class="row">
@@ -461,7 +432,7 @@
                                     </a>
                                     <div class="product-flag">
                                         <ul>
-                                            <li><span class="sale">new</span> </li>
+                                            <li><span class="sale">new</span></li>
                                             <li><span class="discount-percentage">-5%</span></li>
                                         </ul>
                                     </div>
@@ -505,10 +476,14 @@
                             </div>
                             <!-- single-product-end -->
                         </div>
-
+                    @endforeach
+                @else
+                    <p>Không có sản phẩm mới nào.</p>
+                @endif
             </div>
         </div>
     </div>
+
 
 
     <!-- new-book-area-start -->
